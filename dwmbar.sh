f8050b9bbb513c0dae380bbcaded6fcde03d65f5
Jakub Nowak <jakubnowak712@gmail.com>
2019-11-22 23:34:51 +0100

    added bottom status bar with system info

--- dwmbar.sh
+++ dwmbar.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 while true; do
-	xsetroot -name "$(date +"%V %a - %e %b %Y - %T ")"
+	xsetroot -name "$(date +"%V %a - %e %b %Y - %T "); \
+$(acpi -b | sed -e 's/Battery 0: // ;' \
+		-e 's/, / / ; ' \
+		-e 's/Charging /⚡/ ;' \
+		-e 's/Discharging // ;' \
+		-e 's/\([0-9][0-9]\):\([0-6][0-9]\):\([0-6][0-9]\)/\1:\2/'
+	#TODO: `awk` and `/proc/meminfo` instead of `free`
+			) - $(top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}') - MEM $(free | sed -n 2p | awk  '{x = 100 * $3 / $2; printf("%.0f%"), x}' )"
 	sleep 1s
 done
 #—   
+#$(sed 's/$/%/' /sys/class/power_supply/BAT0/capacity)
+#acpi -b | sed -e 's/Battery 0: // ; s/, / / ; s/Charging /⚡/ ; s/Discharging ; s/\([0-9][0-9]\):\([0-6][0-9]\):\([0-6][0-9]\)/\1:\2/'
+#top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}'
+#free -h | sed -n 2p | awk '{print $3}'
