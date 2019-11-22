65fc0ab10a269c30008f5843f56c9fc2e8b5f169
Jakub Nowak <jakubnowak712@gmail.com>
2019-11-22 19:10:43 +0100

    small changes

--- dwmbar.sh
+++ dwmbar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/dash
 
 while true; do
 	xsetroot -name "$(date +"%V %a - %e %b %Y - %T "); \
@@ -8,11 +8,12 @@ $(acpi -b | sed -e 's/Battery 0: // ;' \
 		-e 's/Discharging // ;' \
 		-e 's/\([0-9][0-9]\):\([0-6][0-9]\):\([0-6][0-9]\)/\1:\2/'
 	#TODO: `awk` and `/proc/meminfo` instead of `free`
-			) - $(top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}') - MEM $(free | sed -n 2p | awk  '{x = 100 * $3 / $2; printf("%.0f%"), x}' )"
+			) - $(top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}') $(sensors | awk '/Package id 0:/ {print $4}' | sed s/+//
+			) - MEM $(free | sed -n 2p | awk  '{x = 100 * $3 / $2; printf("%.0f%"), x}')"
 	sleep 1s
 done
-#—   
-#$(sed 's/$/%/' /sys/class/power_supply/BAT0/capacity)
-#acpi -b | sed -e 's/Battery 0: // ; s/, / / ; s/Charging /⚡/ ; s/Discharging ; s/\([0-9][0-9]\):\([0-6][0-9]\):\([0-6][0-9]\)/\1:\2/'
-#top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}'
-#free -h | sed -n 2p | awk '{print $3}'
+# —   
+# $(sed 's/$/%/' /sys/class/power_supply/BAT0/capacity)
+# acpi -b | sed -e 's/Battery 0: // ; s/, / / ; s/Charging /⚡/ ; s/Discharging ; s/\([0-9][0-9]\):\([0-6][0-9]\):\([0-6][0-9]\)/\1:\2/'
+# top -b -n1 | grep -m 1 CPU: | awk '{print "CPU "$2"%"}'
+# free -h | sed -n 2p | awk '{print $3}'
